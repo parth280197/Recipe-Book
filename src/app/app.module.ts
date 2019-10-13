b940181236b0a08c29d871412bb3fcaf168165c5
Parth <parth280197@gmail.com>
2019-10-13 14:25:37 -0500

    Components are reused

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import {HeaderComponent} from "./header/header.component";
 import {RecipesComponent} from "./recipes/recipes.component";
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
 import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
+import {RecipeListComponent} from "./recipes/recipe-list/recipe-list.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipeListComponent
   ],
   imports: [BrowserModule],
   providers: [],
