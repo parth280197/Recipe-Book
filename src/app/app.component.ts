d53df7099d3199fae29a8f8093b68830c8e7935d
Parth <parth280197@gmail.com>
2019-10-19 22:35:56 -0500

    Recipe item component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Project';
+  currentPage = "recipes";
+  title = "Project";
+  onTabChange = (navigationLink: string) => {
+    if (navigationLink == "recipes") {
+      this.currentPage = "recipes";
+    } else if (navigationLink == "shopping list") {
+      this.currentPage = "shopping list";
+    }
+  };
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,11 @@ export class RecipeListComponent implements OnInit {
       "pav bhaji",
       "Pav Bhaji is a very famous Maharashtrian street food that is popular not only in every part of our country but world wide. It is unique and super delicious dish of variety of vegetables that is particularly cooked in butter with special spicy pav bhaji masala.",
       "https://www.cubesnjuliennes.com/wp-content/uploads/2019/10/Pav-Bhaji.jpg"
+    ),
+    new Recipe(
+      "pav bhaji",
+      "Pav Bhaji is a very famous Maharashtrian street food that is popular not only in every part of our country but world wide. It is unique and super delicious dish of variety of vegetables that is particularly cooked in butter with special spicy pav bhaji masala.",
+      "https://www.cubesnjuliennes.com/wp-content/uploads/2019/10/Pav-Bhaji.jpg"
     )
   ];
   constructor() {}
